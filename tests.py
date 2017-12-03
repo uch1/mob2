d69fbb990d0e8b719ec3182a1bcaaaa68296038e
Uchenna  Aguocha <uk.aguocha@gmail.com>
2017-12-02 19:18:09 -0800

    is server working now?

--- tests.py
+++ tests.py
@@ -17,17 +17,69 @@ class TripPlannerTestCase(unittest.TestCase):
       global db
 
       # Reduce encryption workloads for tests
-      server.app.bcrypt_rounds = 4
+      #server.app.bcrypt_rounds = 4
 
-      db = mongo.trip_planner_test
+      db = mongo.test
       server.app.db = db
 
       db.drop_collection('users')
       db.drop_collection('trips')
 
     # User tests, fill with test methods
-    def testCreateUser(self):
-        pass
+
+    def test_get_user(self):
+
+        ## Post 2 users to database
+        self.app.post('/user/',
+                      headers=None,
+                      data=json.dumps(dict(
+                                           name="Uchenna Aguocha",
+                                           email="uk.aguocha@example.com"
+                                           )),
+                                           content_type='application/json')
+
+        ## 3 Make a get request to fetch the posted user
+        response = self.app.get('/user/',query_string=dict(email="uk.aguocha@example.com"))
+
+        # Decode reponse
+        response_json = json.loads(response.data.decode())
+
+                      ## Actual test to see if GET request was succesful
+                      ## Here we check the status code
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_user(self):
+        self.app.post('/user/',
+                        headers=None,
+                        data=json.dumps(dict(
+                                                name="Goku Son",
+                                                email="goku.son@example.com")),
+                                                content_type='application/json')
+        ## Actual test to see if GET request was succesful
+        ## Here we check the status code
+        self.assertEqual(response.status_code, 200)
+
+    def test_put_user(self):
+
+        ## Post 2 users to database
+        self.app.post('/user/',
+                        headers=None,
+                        data=json.dumps(dict(
+                                                name="Goku Son",
+                                                email="goku.son@example.com")),
+                                                content_type='application/json')
+
+        ## 3 Make a get request to fetch the posted user
+        response = self.app.get('/user', query_string=dict(email="goku.son@example.com"))
+
+        # Decode reponse
+        response_json = json.loads(response.data.decode())
+
+        ## Actual test to see if GET request was succesful
+        ## Here we check the status code
+        self.assertEqual(response.status_code, 200)
+    def test_delete_user(self):
 
 if __name__ == '__main__':
     unittest.main()
